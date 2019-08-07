90ea6173820b4a69357834bd464a9955372ea1d9
Diljit VJ <diljitvj@gmail.com>
2019-08-07 12:19:42 +0530

    Add Day 1 Assignments

---
+++ Assignment-Day-1.js
@@ -0,0 +1,43 @@
+// Deeksha
+
+// Question 1
+5 * 2 + (0 || 10) * 40;
+// Question 2
+1 > 2 > 4 < 6;
+// Question 3
+"" && 6 || {} && !0
+// Hitha
+
+// Question 1
+10 - 8 + (6 && 10) / 6;
+// Question 2
+10 < 6 > 2 > 16;
+// Question 3
+!"" && 2 || true && false
+
+// Nishmitha
+
+// Question 1
+5 * 7 + (15 && 0) % 2;
+// Question 2
+1 > 6 < 10 < -1;
+// Question 3
+5 || 0 && 10 && true
+
+// Prajwal
+
+// Question 1
+20 / 5 + (-1 || 0 - 2);
+// Question 2
+15 < 2 > 5 < 6;
+// Question 3
+10 && !6 || 100 || 6
+
+// Varsha
+
+// Question 1
+16 * 3 / (5 && 2) / 3;
+// Question 2
+15 < 2 > 5 < 6;
+// Question 3
+2 || -1 && 4 || 10
